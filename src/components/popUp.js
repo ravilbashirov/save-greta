d98abc1a2ed8b0c6563123cebeafb0ce0290ae75
Ravil Bashirov <ravilbashirov@MacBook-Pro-Ravil.local>
2020-05-29 10:56:45 +0300

    added verstka btn

--- src/components/popUp.js
+++ src/components/popUp.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
 export const PopUp = () => {
+    const onClickBtn = () => {
+        console.log("Click")
+    }
     return (
         <div class="popup">
             <p>Title</p>
             <div class="popup_inner"></div>
-            <button>Начать игру</button>
+            <button onClick={onClickBtn}>Начать игру</button>
         </div>
     )
 }
